f3a49cf3d30ffa2541ac6def73163f08a2f5d159
Emma Scanlon <emma.scanlon@extendhealth.com>
2022-04-07 08:05:13 -0600

    some other stuff

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+    // entry
+    entry: "./src/js/app.js",
+    // output
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle-[contenthash].js"
+    },
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    // plugins
+    plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({template: "./template.html" })]
+
+}
